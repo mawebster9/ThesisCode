734e6a21464ab3d5d546195b70b0e7df0730ebaf
Michaela Webster <mawebster9@yahoo.com>
2019-04-05 15:11:37 -0400

    Basic machine learning (linear regression) is done on drugs labels. Still need to work on the prediction stuff. Plan to fix the recency label. Need to save prediction on X_new and its predicted outcome of denied to csv file and evaluate model.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ThesisCode
 This project will build off of the work of Sarah Miller and will delve into the Adjudicative Guidelines and the security clearance appeals cases. This particular body of work will try to take case input data and try to determine if the case was denied or overturned.
+
+
+Requirements:
+Run the following commands before running code
+
+pip install pandas
+pip install -U scikit-learn scipy matplotlib
+
+
+To run:
+1. open the command prompt
+2. type python '[File_Path_to_thesis.py'
+
--- thesis.py
+++ thesis.py
@@ -1,4 +1,57 @@
 # Michaela Webster's Thesis Project #
 """ This project aims to try to predict the outcome of security clearance appeals cases """
 
+#use this link for scikit learn help: https://www.ritchieng.com/pandas-scikit-learn/
+
+# To support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+
+# imports
+import os
+import pandas as pd
+import warnings
+import matplotlib.pyplot as plt
+import numpy as np
+import subprocess
+import shlex
+
+#open drugs_query.csv as pandas dataframe
+path_to_file = "C:\\Users\\maweb\\Documents\\ThesisCode\\drugs_query.csv"
+data = pd.read_csv(path_to_file, encoding='latin-1')
+
+#print bar graph of drugs yes/no vs denied yes/no
+print (data.groupby(['Drugs', 'Denied']).size())
+
+#print bar graph to show drugs/denied
+
+
+# set the featured columns for X (Drugs)
+feature_cols = ['Drugs']
+# you want all rows, and the feature_cols' columns
+X = data.loc[:, feature_cols]
+# now we want to create our response vector
+y = data.Denied
+
+
+# START DOING MACHINE LEARNING STUFF
+# 1. import
+from sklearn.linear_model import LogisticRegression
+# 2. instantiate model
+logreg = LogisticRegression(penalty='l1',dual=False,max_iter=110, solver='liblinear')
+# 3. fit 
+logreg.fit(X, y)
+
+
+# FOR TEST DATA
+#open drugs_test.csv as pandas dataframe
+path_to_file = "C:\\Users\\maweb\\Documents\\ThesisCode\\drugs_test.csv"
+data2 = pd.read_csv(path_to_file, encoding='latin-1')
+X_new = data.loc[:, feature_cols]
+new_pred_class = logreg.predict(X_new)
+
+#SAVE DATAFRAME TO CSV
+# pandas would align them next to each other
+# to ensure the first column is PassengerId, use .set_index
+#drug_data = pd.DataFrame({'Drugs':test.PassengerId, 'Denied':new_pred_class}).set_index('Drugs')
+#drug_data.to_csv('drug_df.csv')
 
